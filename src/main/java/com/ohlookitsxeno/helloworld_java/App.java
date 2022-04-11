4888df15be95026eb00077c62a7851aa894caf46
ohlookitsxeno <djxavieriii@gmail.com>
2022-04-11 01:02:44 -0500

    added even-odd testing feature

--- src/main/java/com/ohlookitsxeno/helloworld_java/App.java
+++ src/main/java/com/ohlookitsxeno/helloworld_java/App.java
@@ -1,5 +1,7 @@
 package com.ohlookitsxeno.helloworld_java;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,13 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Type a number");
+
+        int num = sc.nextInt();
+
+        System.out.println((num % 2 == 0) ? "even" : "odd");
     }
 }
